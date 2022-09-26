dc19e6f4d3218c20353a3d6d878d292029a453b7
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2022-09-26 20:41:39 +0530

    Time: 8 ms (69.30%), Space: 11.4 MB (70.01%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,21 +1,27 @@
 class Solution {
 private:
-    vector<int> parent;
+    vector<int>parent;
     int find(int node){
         return parent[node] == -1 ? node : parent[node] = find(parent[node]);
     }
-    void setUnion(int a, int b){
-        int x = find(a), y = find(b);
-        if(x != y) parent[x] = y;
+    void setUnion(int a,int b){
+        int x = find(a),y = find(b);
+        if(x!=y){
+            parent[x] = y;
+        }
     }
 public:
     bool equationsPossible(vector<string>& equations) {
-        parent.resize(26, -1);
-        for(auto eq : equations){
-            if(eq[1] == '=') setUnion(eq[0] - 'a', eq[3] - 'a');
+        parent.resize(26,-1);
+        for(auto it : equations){
+            if(it[1]=='='){
+                setUnion(it[0]-'a',it[3]-'a');
+            }
         }
-        for(auto eq : equations){
-            if(eq[1] == '!' && find(eq[0] - 'a') == find(eq[3] - 'a')) return false;
+        for(auto it : equations){
+            if(it[1]=='!' and find(it[0]-'a')==find(it[3]-'a')){
+                return  false;
+            }
         }
         return true;
     }
