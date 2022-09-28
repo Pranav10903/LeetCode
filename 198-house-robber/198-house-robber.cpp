54647d4945ff5a75e1b8478546e45009bd1c5720
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2022-09-28 11:53:49 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (77.77%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i=1;i<n;i++){
+            int take = nums[i];
+            if(i>1) take += prev2;
+            int nottake = 0 + prev;
+            int cur = max(take,nottake);
+            prev2 = prev;
+            prev = cur;
+        }
+        return prev;
+    }
+};
