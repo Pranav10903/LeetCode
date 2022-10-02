0a163fc78c96ac6d66f32f1d82a3bab863dd4e4e
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2022-10-02 20:39:31 +0530

    Time: 1340 ms (5.01%), Space: 0B (100.00%) - LeetHub

---
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.recordDate,w.recordDate) =  1
+        AND weather.Temperature > w.Temperature
