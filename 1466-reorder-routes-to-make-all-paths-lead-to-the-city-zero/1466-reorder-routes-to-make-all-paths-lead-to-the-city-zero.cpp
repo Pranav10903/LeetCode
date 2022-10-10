552d2958bf7c5b86a2fafe3213a2fb7ab1e3614b
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2022-10-10 17:42:05 +0530

    Time: 553 ms (77.13%), Space: 115.9 MB (49.36%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -5,28 +5,28 @@ public:
         vector<int>neighbour[n],adj[n];
         for(auto it:arr){
             neighbour[it[0]].push_back(it[1]);
-            neighbour[it[1]].push_back(it[0]);
-            adj[it[0]].push_back(it[1]);
+            neighbour[it[1]].push_back(it[0]); // creating a neighbour list containing all neighbours
+            adj[it[0]].push_back(it[1]); // creating an adj list based on connections array to check reachability
         }
         queue<int>q;
-        vector<int>vis(n,0);
+        vector<int>visited(n,0);
         q.push(0);
         while(!q.empty()){
             int node = q.front();
-            vis[node] = 1;
+            visited[node] = 1;          //BFS
             q.pop();
             for(auto Neighbour:neighbour[node]){
-                if(!vis[Neighbour]){
-                    bool canReach = false;
-                    for(auto AdjacenttoNeighbour:adj[Neighbour]){
-                        if(AdjacenttoNeighbour == node){
-                            canReach = true;
+                if(!visited[Neighbour]){
+                    bool canReach = false;   //Checking if neighbours of current node are visited
+                    for(auto AdjacenttoNeighbour:adj[Neighbour]){ // if not visited check if the (prev node) i.e node itself is reachable from the neighbour through adj[] list.
+                        if(AdjacenttoNeighbour == node){  
+                            canReach = true;  // if reachable dont increment ans
                             q.push(Neighbour);
                             break;
                         }
                     }
                     if(!canReach) {
-                        ans++;
+                        ans++;  // if not reachable increment ans and push neighbour into the queue
                         q.push(Neighbour);
                     }
                 }
