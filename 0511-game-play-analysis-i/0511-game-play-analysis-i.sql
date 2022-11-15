20c802df8e129cbe0fa8998d425f1831ff4374ff
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2022-11-15 19:25:56 +0530

    Time: 660 ms (91.03%), Space: 0B (100.00%) - LeetHub

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+SELECT
+  X.player_id,
+  X.event_date AS first_login
+FROM
+  (
+    SELECT
+      A.player_id,
+      A.event_date,
+      RANK() OVER (
+        PARTITION BY
+          A.player_id
+        ORDER BY
+          A.event_date
+      ) AS rnk
+    FROM
+      Activity A
+  ) X
+WHERE
+  X.rnk = 1;
