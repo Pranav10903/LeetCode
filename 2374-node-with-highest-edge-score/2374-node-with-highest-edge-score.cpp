9b047ba22da0b75f983aa4b05653631ab1ccc072
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2022-11-20 15:22:52 +0530

    Time: 352 ms (75.19%), Space: 139.9 MB (19.51%) - LeetHub

--- 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     int edgeScore(vector<int>& edges) {
-        int N = edges.size();
-        vector<int> adj[N];
-        for(int i = 0;i<N;i++){
-            adj[edges[i]].push_back(i);
+        // int N = edges.size();
+        // vector<int> adj[N];
+        // for(int i = 0;i<N;i++){
+        //     adj[edges[i]].push_back(i);
+        // }
+        // long long int ans = INT_MIN, res = 0;
+        // for(int i=0;i<N;i++){
+        //     long long int sum = 0;
+        //     for(auto it : adj[i]){
+        //         sum+=it;
+        //     }
+        //     if(ans<sum){
+        //         ans = sum;
+        //         res = i;
+        //     }
+        // }
+        // return res;
+         unordered_map<int,long long> mp;
+        for(int i=0;i<edges.size();i++){
+            mp[edges[i]] += i;
         }
-        long long int ans = INT_MIN, res = 0;
-        for(int i=0;i<N;i++){
-            long long int sum = 0;
-            for(auto it : adj[i]){
-                sum+=it;
-            }
-            if(ans<sum){
-                ans = sum;
-                res = i;
-            }
+        int ans = INT_MAX;
+        long long mx = INT_MIN;
+        for(auto &x : mp){
+            mx = max(mx,x.second);
         }
-        return res;
+        
+        for(auto &x : mp){
+            if(x.second == mx) ans = min(ans,x.first);
+        }
+        
+        return ans;
     }
 };
