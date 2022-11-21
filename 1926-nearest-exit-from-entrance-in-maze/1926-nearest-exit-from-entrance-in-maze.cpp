15ec1ff402065266d716b988f4a9c8a4b1690ca7
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2022-11-22 01:38:54 +0530

    Time: 303 ms (50.46%), Space: 30.7 MB (53.03%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int rows = int(maze.size()), cols = int(maze[0].size());
-        vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
-        // Mark the entrance as visited since its not a exit.
-        int startRow = entrance[0], startCol = entrance[1];
-        maze[startRow][startCol] = '+';
-        
-        // Start BFS from the entrance, and use a queue `queue` to store all 
-        // the cells to be visited.
-        queue<array<int, 3>> queue;
-        queue.push({startRow, startCol, 0});
-        
-        while (!queue.empty()) {
-            auto [currRow, currCol, currDistance] = queue.front();
-            queue.pop();
-            
-            // For the current cell, check its four neighbor cells.
-            for (auto dir : dirs) {
-                int nextRow = currRow + dir.first, nextCol = currCol + dir.second;
-
-                // If there exists an unvisited empty neighbor:
-                if (0 <= nextRow && nextRow < rows && 0 <= nextCol && nextCol < cols \
-                   && maze[nextRow][nextCol] == '.') {
-                    
-                    // If this empty cell is an exit, return distance + 1.
-                    if (nextRow == 0 || nextRow == rows - 1 || nextCol == 0 || nextCol == cols - 1)
-                        return currDistance + 1;
-                    
-                    // Otherwise, add this cell to 'queue' and mark it as visited.
-                    maze[nextRow][nextCol] = '+';
-                    queue.push({nextRow, nextCol, currDistance + 1});
+    int nearestExit(vector<vector<char>>& maze, vector<int>& ent) {
+        int n=maze.size(),m=maze[0].size();
+        vector<vector<bool>>visited(n,vector<bool>(m,0));
+        queue<pair<int,int>>q;
+        q.push({ent[0],ent[1]});
+        visited[ent[0]][ent[1]]=1;
+        int step=0;
+        int dx[]={1,0,-1,0};
+        int dy[]={0,1,0,-1};
+        while(!q.empty()){
+            int size=q.size();
+            step++;
+            while(size--){
+                pair<int,int>p=q.front();q.pop();
+                int row=p.first;
+                int col=p.second;
+                for(int i=0;i<4;i++){
+                    int nrow=row+dx[i];
+                    int ncol=col+dy[i];
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                        if(maze[nrow][ncol]!='.')continue;
+                        if(visited[nrow][ncol]) continue;
+                        if(nrow==0 or nrow==n-1 or ncol==0 or ncol==m-1)return step;
+                        visited[nrow][ncol]=1;
+                        q.push({nrow,ncol});
+                    }
                     
                 }
             }
         }
-        
-        // If we finish iterating without finding an exit, return -1.
         return -1;
     }
 };
