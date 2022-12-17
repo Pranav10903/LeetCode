faabc50d6d95bbefb3b4fcf0cc1cd5f602228d9c
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2022-12-17 17:59:19 +0530

    Time: 7 ms (96.48%), Space: 14 MB (79.86%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int i, int j,int val, int newColor)
-    {
-        if(i<0 || i>=image.size() || j<0 || j>= image[0].size() || image[i][j] == newColor || image[i][j] != val)
-        {
+    void dfs(vector<vector<int>> &image, int r, int c, int val, int newC){
+        if(r<0 || r>=image.size() || c<0 ||c>=image[0].size() || image[r][c]== newC || image[r][c]!=val ){
             return;
         }
-        image[i][j] = newColor;
-        dfs(image,i-1,j,val,newColor);
-        dfs(image,i+1,j,val,newColor);
-        dfs(image,i,j-1,val,newColor);
-        dfs(image,i,j+1,val,newColor);
+        image[r][c] = newC;
+        dfs(image,r+1,c,val,newC);
+        dfs(image,r-1,c,val,newC);
+        dfs(image,r,c+1,val,newC);
+        dfs(image,r,c-1,val,newC);         
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)     {
         int val = image[sr][sc];
