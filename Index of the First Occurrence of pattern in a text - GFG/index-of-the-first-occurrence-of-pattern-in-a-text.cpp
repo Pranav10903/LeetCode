f2aa376528035026f972867352bce7852739093f
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2023-01-01 22:54:52 +0530

    Added solution - LeetHub

---
+++ Index of the First Occurrence of pattern in a text - GFG/index-of-the-first-occurrence-of-pattern-in-a-text.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int findMatching(string haystack, string needle) {
+        // Code here
+        int m = haystack.size(), n = needle.size();
+        for (int i = 0; i <= m - n; i++) {
+            int j = 0;
+            for (j = 0; j < n; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    break;
+                }
+            }
+            if (j == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string text, pat;
+        cin >> text >> pat;
+        Solution obj;
+        cout << obj.findMatching(text, pat) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
