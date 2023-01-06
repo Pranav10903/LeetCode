f8a21fd831a1dfd4ea2896eae801c03b959b91ae
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2023-01-06 15:56:17 +0530

    Time: 320 ms (43.52%), Space: 80.3 MB (5.70%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
-        int count = 0;
-        for(auto it:costs){
-            if(it<=coins){
-                coins-=it;
-                cout<<it<<' ';
-                count++;
-            }
-            else break;
+        int m = *max_element(costs.begin(),costs.end());
+        int n = costs.size();
+        int ans = 0;
+        vector<int> mp(m+1,0);
+        for(int i=0;i<n;i++){
+            mp[costs[i]]++;
         }
-        return count;
-    }
+        for(int i=1;i<=m;i++){
+            if(mp[i]==0){
+                continue;
+            }
+            if(coins < i){
+                break;
+            }
+            int count = min(mp[i],coins/i);
+            coins = coins - i*count;
+            ans += count;
+        }
+        return ans;
+    }    
 };
