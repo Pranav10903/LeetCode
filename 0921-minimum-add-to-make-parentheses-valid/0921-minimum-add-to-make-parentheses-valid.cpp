cb60c723f6f7c416a9fd3434e0948747571391c2
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2023-01-18 18:50:05 +0530

    Time: 4 ms (41.75%), Space: 6.2 MB (91.09%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int open = 0;
-        int close = 0;
-        for(int i=0;i<s.length();i++){ 
+        int n = s.length();
+        int open,close;
+        open = close = 0;
+        for(int i=0;i<n;i++){
             if(s[i]==')'){
                 if(open==0){
                     close++;
@@ -16,6 +17,6 @@ public:
                 open++;
             }
         }
-        return open + close;
+        return open+close;
     }
 };
