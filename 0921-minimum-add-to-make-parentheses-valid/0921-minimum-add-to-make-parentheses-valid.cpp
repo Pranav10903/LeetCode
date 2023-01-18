22d058e4ad39a382858407303d006f44bb563a59
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2023-01-18 18:47:07 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (64.98%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -3,7 +3,7 @@ public:
     int minAddToMakeValid(string s) {
         int open = 0;
         int close = 0;
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.length();i++){ 
             if(s[i]==')'){
                 if(open==0){
                     close++;
