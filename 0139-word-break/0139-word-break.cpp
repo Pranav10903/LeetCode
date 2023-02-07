2c4e445f09cbca261f755c2afd308bb40eda8e18
Pranav Tatavarthy <97344709+Pranav10903@users.noreply.github.com>
2023-02-08 01:08:54 +0530

    Time: 4 ms (87.67%), Space: 9.5 MB (63.59%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict;
+        for(auto it : wordDict){
+            dict.insert(it);
+        }
+        if(dict.size()==0) return false;
+        
+        vector<bool> dp(s.size()+1,false);
+        dp[0]=true;
+        
+        for(int i=1;i<=s.size();i++)
+        {
+            for(int j=i-1;j>=0;j--)
+            {
+                if(dp[j])
+                {
+                    string word = s.substr(j,i-j);
+                    if(dict.find(word)!= dict.end())
+                    {
+                        dp[i]=true;
+                        break; //next i
+                    }
+                }
+            }
+        }
+        
+        return dp[s.size()];
+    }
+};
